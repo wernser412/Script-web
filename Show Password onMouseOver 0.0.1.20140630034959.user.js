39e79ff1ce21689d6fac9db8522a9abf0f71bddb
wernser412 <53667212+wernser412@users.noreply.github.com>
2020-11-15 18:00:16 -0500

    Add files via upload

---
+++ Show Password onMouseOver 0.0.1.20140630034959.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name          Show Password onMouseOver
+// @namespace     http://zoolcar9.lhukie.net/
+// @include       *
+// @description	  Show password when mouseover on password field
+// @author        LouCypher
+// @license       free
+// @version 0.0.1.20140630034959
+// ==/UserScript==
+
+window.setTimeout(function() {
+  var passFields = document.querySelectorAll("input[type='password']");
+  if (!passFields.length) return;
+  for (var i = 0; i < passFields.length; i++) {
+    passFields[i].addEventListener("mouseover", function() {
+      this.type = "text";
+    }, false);
+    passFields[i].addEventListener("mouseout", function() {
+      this.type = "password";
+    }, false);
+  }
+}, 1000)
